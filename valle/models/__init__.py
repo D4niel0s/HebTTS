4b977d30b4a269c1786fe5455724cca1e150ef62
D4niel0s <das.volkov@gmail.com>
2025-04-04 21:49:24 +0300

    no BOS

--- valle/models/__init__.py
+++ valle/models/__init__.py
@@ -71,7 +71,7 @@ def add_model_arguments(parser: argparse.ArgumentParser):
     )
     parser.add_argument(
         "--prepend-bos",
-        default=True,
+        default=False,
         help="Whether prepend <BOS> to the acoustic tokens -> AR Decoder inputs.",
     )
     parser.add_argument(
