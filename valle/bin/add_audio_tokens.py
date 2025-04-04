b3352246eb136d22e4e8e0080541a2b1be85b874
Shahar Glam <shahar@Shahars-MacBook-Air.local>
2025-04-04 12:55:12 +0300

    first fail of the daygit add .

---
+++ valle/bin/add_audio_tokens.py
@@ -0,0 +1,181 @@
+#!/usr/bin/env python3
+# Copyright    2023                            (authors: Feiteng Li)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Phonemize Text and EnCodec Audio.
+
+Usage example:
+    python3 bin/tokenizer.py \
+        --src_dir ./data/manifests --output_dir ./data/tokenized
+
+"""
+import argparse
+import logging
+import os
+from pathlib import Path
+
+import torch
+import torch.multiprocessing
+from icefall.utils import get_executor
+from lhotse import CutSet, NumpyHdf5Writer
+from lhotse.recipes.utils import read_manifests_if_cached
+from tqdm.auto import tqdm
+
+from valle.data import (
+    AudioTokenConfig,
+    AudioTokenExtractor,
+)
+
+os.environ["PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION"] = "python"
+
+
+# Torch's multithreaded behavior needs to be disabled or
+# it wastes a lot of CPU and slow things down.
+# Do this outside of main() in case it needs to take effect
+# even when we are not invoking the main (e.g. when spawning subprocesses).
+torch.set_num_threads(1)
+torch.set_num_interop_threads(1)
+torch.multiprocessing.set_sharing_strategy("file_system")
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--src-dir",
+        type=Path,
+        default=Path("/home/yandex/APDL2425a/group_6/Documents"),
+        help="Path to the manifest files",
+    )
+    parser.add_argument(
+        "--output-dir",
+        type=Path,
+        default=Path("/home/yandex/APDL2425a/group_6/Documents"),
+        help="Path to the tokenized files",
+    )
+    parser.add_argument(
+        "--audio-extractor",
+        type=str,
+        default="Encodec",
+        help="Encodec or Fbank",
+    )
+    parser.add_argument(
+        "--dataset-parts",
+        type=str,
+        default="all",
+        help="Space separated dataset parts",
+    )
+    parser.add_argument(
+        "--prefix",
+        type=str,
+        default="",
+        help="prefix of the manifest file",
+    )
+    parser.add_argument(
+        "--suffix",
+        type=str,
+        default="jsonl.gz",
+        help="suffix of the manifest file",
+    )
+    parser.add_argument(
+        "--batch-duration",
+        type=float,
+        default=400.0,
+        help="The maximum number of audio seconds in a batch."
+        "Determines batch size dynamically.",
+    )
+
+    return parser.parse_args()
+
+
+def main():
+    args = get_args()
+
+    dataset_parts = args.dataset_parts.replace("--dataset-parts", "").strip()
+    if dataset_parts == "all":  # LibriTTS
+        dataset_parts = [
+            "dev",
+            "test",
+            "train"
+        ]
+    else:
+        dataset_parts = dataset_parts.replace("-p", "").strip().split(" ")
+
+    assert len(dataset_parts) >= 1
+
+    manifests = read_manifests_if_cached(
+        dataset_parts=dataset_parts,
+        output_dir=args.src_dir,
+        prefix=args.prefix,
+        suffix=args.suffix,
+        types=["recordings", "supervisions", "cuts"],
+    )
+
+    audio_extractor = None
+    if args.audio_extractor == "Encodec":
+        audio_extractor = AudioTokenExtractor(AudioTokenConfig())
+
+    Path(args.output_dir).mkdir(parents=True, exist_ok=True)
+    
+    num_jobs = min(32, os.cpu_count())
+    logging.info(f"dataset_parts: {dataset_parts} manifests {len(manifests)}")
+
+    with get_executor() as ex:
+        for partition, m in manifests.items():
+            logging.info(
+                f"Processing partition: {partition} CUDA: {torch.cuda.is_available()}"
+            )
+            try:
+                cut_set = CutSet.from_manifests(
+                    recordings=m["recordings"],
+                    supervisions=m["supervisions"],
+                )
+            except Exception:
+                print("tell me why")
+                cut_set = m["cuts"]
+
+            storage_path = (
+                f"{args.output_dir}/encodec_{partition}"
+            )
+
+            with torch.no_grad():
+                if (torch.cuda.is_available()):
+                    cut_set = cut_set.compute_and_store_features_batch(
+                        extractor=audio_extractor,
+                        storage_path=storage_path,
+                        num_workers=num_jobs,
+                        batch_duration=args.batch_duration,
+                        collate=False,
+                        overwrite=True,
+                        storage_type=NumpyHdf5Writer,
+                    )
+                else:
+                    cut_set = cut_set.compute_and_store_features(
+                        extractor=audio_extractor,
+                        storage_path=storage_path,
+                        num_jobs=num_jobs if ex is None else 64,
+                        executor=ex,
+                        storage_type=NumpyHdf5Writer,
+                    )
+
+            cuts_filename = f"cuts_{partition}.{args.suffix}"
+            cut_set.to_file(f"{args.output_dir}/{cuts_filename}")
+
+
+if __name__ == "__main__":
+    formatter = (
+        "%(asctime)s %(levelname)s [%(filename)s:%(lineno)d] %(message)s"
+    )
+    logging.basicConfig(format=formatter, level=logging.INFO)
+    main()
