eef39df0e9466dd878cc0e14136aa790e77006fb
D4niel0s <das.volkov@gmail.com>
2025-04-06 14:45:39 +0300

    s

--- valle/models/valle.py
+++ valle/models/valle.py
@@ -2001,12 +2001,10 @@ class VALLE_ALEPHBERT_CONCAT(VALLF):
             xy_pos = torch.concat([x, y_pos], dim=1)
             print(f'{xy_pos.shape=}')
 
-            print(f'{xy_padding_mask=}')
-
             xy_dec, _ = self.ar_decoder(
                 xy_pos,
-                xy_attn_mask[0].bool(),
-                xy_padding_mask,
+                xy_attn_mask.bool(),
+                ar_xy_padding_mask,
                 True,
             )
 
