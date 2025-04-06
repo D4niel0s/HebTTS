1365c15e695b4e2157c94419129a3da61e723d0f
D4niel0s <das.volkov@gmail.com>
2025-04-06 18:14:18 +0300

    sws

--- valle/models/valle.py
+++ valle/models/valle.py
@@ -2003,7 +2003,7 @@ class VALLE_ALEPHBERT_CONCAT(VALLF):
             
             xy_dec, _ = self.ar_decoder(
                 xy_pos,
-                xy_attn_mask,
+                xy_attn_mask[0].bool(),
                 ar_xy_padding_mask,
                 True
             )
