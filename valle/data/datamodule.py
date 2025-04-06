54b976fb8f931b6b47e20f1d835b34bd2e0ac264
D4niel0s <das.volkov@gmail.com>
2025-04-06 22:01:12 +0300

    sw

--- valle/data/datamodule.py
+++ valle/data/datamodule.py
@@ -98,7 +98,7 @@ class TtsDataModule:
         group.add_argument(
             "--max-duration",
             type=int,
-            default=40.0,
+            default=50,
             help="Maximum pooled recordings duration (seconds) in a "
             "single batch. You can reduce it if it causes CUDA OOM.",
         )
