867c5d56a18d25ba8b65350c16ab47e20f98de54
D4niel0s <das.volkov@gmail.com>
2025-04-10 22:58:45 +0300

    dbg

--- valle/bin/add_tokens.py
+++ valle/bin/add_tokens.py
@@ -35,6 +35,7 @@ def append_chars_subwords(output_dir):
         new_cut_list = list()
 
         for c in tqdm(cuts, "tokenizing text"):
+            print(f'{c.supervisions[0].text=}')
             text = c.supervisions[0].text
 
             char_tokens = tokenize_text(chars_tokenizer, text=text)
