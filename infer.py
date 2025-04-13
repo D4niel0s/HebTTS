eb36ef273efb987408bb75e65ca60eec722cca4b
D4niel0s <das.volkov@gmail.com>
2025-04-13 12:01:08 +0300

    epoch 2 NAR

--- infer.py
+++ infer.py
@@ -155,7 +155,7 @@ def get_args():
     parser.add_argument(
         "--checkpoint",
         type=str,
-        default="/home/yandex/APDL2425a/group_6/Documents/HebTTS/valle/exp/valle_dev/epoch-1.pt",
+        default="/home/yandex/APDL2425a/group_6/Documents/HebTTS/valle/exp/valle_dev/epoch-2.pt",
         help="Path to the saved checkpoint.",
     )
 
