c6da2cbdd91e9df6e3b38e652e9e92b002fd4fad
D4niel0s <das.volkov@gmail.com>
2025-04-13 12:04:04 +0300

    text

--- infer.py
+++ infer.py
@@ -127,7 +127,7 @@ def get_args():
     parser.add_argument(
         "--text",
         type=str,
-        default="חג פסח שמח לכל בית ישראל אחים שלנו היקרים",
+        default="ברוכימ הבאימ לפודקאסט עושים היסטוריה",
         help="Text to be synthesized.",
     )
 
