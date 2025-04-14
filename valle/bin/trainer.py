5368a21dbba66e005ce297157b182ba758351551
D4niel0s <das.volkov@gmail.com>
2025-04-14 22:19:28 +0300

    fewer validations

--- valle/bin/trainer.py
+++ valle/bin/trainer.py
@@ -206,7 +206,7 @@ def get_parser():
     parser.add_argument(
         "--valid-interval",
         type=int,
-        default=30000,
+        default=50000,
         help="""Run validation if batch_idx %% valid_interval is 0.""",
     )
 
