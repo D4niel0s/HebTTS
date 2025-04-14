fb62b4c2adb117e12064fb8699fcd2ae4a9635c1
D4niel0s <das.volkov@gmail.com>
2025-04-14 16:55:44 +0300

    --num-epochs

--- valle/bin/trainer.py
+++ valle/bin/trainer.py
@@ -132,7 +132,7 @@ def get_parser():
     parser.add_argument(
         "--start-batch",
         type=int,
-        default=0,
+        default=70000,
         help="""If positive, --start-epoch is ignored and
         it loads the checkpoint from exp-dir/checkpoint-{start_batch}.pt
         """,
@@ -206,7 +206,7 @@ def get_parser():
     parser.add_argument(
         "--valid-interval",
         type=int,
-        default=25000,
+        default=30000,
         help="""Run validation if batch_idx %% valid_interval is 0.""",
     )
 
