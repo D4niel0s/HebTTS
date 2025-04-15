201d268a9d3244f19e58e9d45bf3538b49581a7f
D4niel0s <das.volkov@gmail.com>
2025-04-16 02:16:24 +0300

    90knar

--- infer.py
+++ infer.py
@@ -156,7 +156,7 @@ def get_args():
     parser.add_argument(
         "--checkpoint",
         type=str,
-        default="/home/yandex/APDL2425a/group_6/Documents/HebTTS/valle/exp/valle_dev/checkpoint-80000.pt",
+        default="/home/yandex/APDL2425a/group_6/Documents/HebTTS/valle/exp/valle_dev/checkpoint-90000.pt",
         help="Path to the saved checkpoint.",
     )
 
