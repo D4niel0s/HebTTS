f39cd27d7bed17478cf2265b2575bbc36b025092
D4niel0s <das.volkov@gmail.com>
2025-04-18 11:33:40 +0300

    250k fixed ar

--- infer.py
+++ infer.py
@@ -156,7 +156,7 @@ def get_args():
     parser.add_argument(
         "--checkpoint",
         type=str,
-        default="/home/yandex/APDL2425a/group_6/Documents/HebTTS/valle/exp/valle_dev/epoch-4.pt",
+        default="/home/yandex/APDL2425a/group_6/Documents/HebTTS/valle/exp/valle_dev/last_low_valid/checkpoint-250000.pt",
         help="Path to the saved checkpoint.",
     )
 
