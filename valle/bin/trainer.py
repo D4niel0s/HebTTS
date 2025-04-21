377fd129b8c91b4deab7727dd2e0c382db471bc6
D4niel0s <das.volkov@gmail.com>
2025-04-21 12:21:30 +0300

    lmfao

--- valle/bin/trainer.py
+++ valle/bin/trainer.py
@@ -122,7 +122,7 @@ def get_parser():
     parser.add_argument(
         "--start-epoch",
         type=int,
-        default=0,
+        default=1,
         help="""Resume training from this epoch. It should be positive.
         If larger than 1, it will load checkpoint from
         exp-dir/epoch-{start_epoch-1}.pt
